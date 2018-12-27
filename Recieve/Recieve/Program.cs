4efe9cc887f27d1478678be69170fbc59b7af48d
Aman <amanchoudhary.com@gmail.com>
2018-12-27 14:08:18 +0530

    Azure Eventhub Reciever Repository Added

---
+++ Recieve/Recieve/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using System.Configuration;
+namespace Recieve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get the values from App.config file
+            string eventHubConnectionString = ConfigurationManager.AppSettings["EventhubConnectionString"];
+            string eventHubName = ConfigurationManager.AppSettings["EventhubName"];
+            string storageAccountName = ConfigurationManager.AppSettings["StorageAccountName"];
+            string storageAccountKey = ConfigurationManager.AppSettings["StorageAccountKey"];
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", storageAccountName, storageAccountKey);
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            //Create the EventProcessorHost with the supplied eventhub configuration with default consumer group ,
+            //we can change the consumer group name based on the needs
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
+            Console.WriteLine("Registering EventProcessor...");
+            var options = new EventProcessorOptions();
+            options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>(options).Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
